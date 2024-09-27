23f6aebe90576091d9f9d399f6f31f367b69593c
Reginah201 <reginahmakgomo2@gmail.com>
2024-09-27 09:36:28 +0200

    Add project files.

---
+++ MilkSong/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MilkSong
+{
+    class MilkSong
+    {
+        public void Sing()
+        {
+            for (int bottles = 99; bottles > 0; bottles--)
+            {
+                if (bottles > 1)
+                {
+                    Console.WriteLine($"{bottles} bottles of milk on the wall, {bottles} bottles of milk.");
+                    Console.WriteLine($"Take one down and pass it around, {bottles - 1} bottles of milk on the wall.\n");
+                }
+                else
+                {
+                    Console.WriteLine($"{bottles} bottle of milk on the wall, {bottles} bottle of milk.");
+                    Console.WriteLine("Take one down and pass it around, no more bottles of milk on the wall.\n");
+                }
+            }
+             
+        }
+    }
+}
